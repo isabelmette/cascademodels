21f91a9c04ee897b98f6a858cf0098ee0fdb909e
Isabel Kloumann <imk36@cornell.edu>
2014-05-06 17:35:34 -0400

    scripting updates

--- params1.py
+++ params1.py
@@ -5,8 +5,8 @@ runtag = 'testing'
 datadir = basedatadir + runtag + "/"
 picdir = basepicdir + runtag + "/"
 
-bs = [0.01, 0.1, 0.5, 1.1, 1.5, 2.0, 2.5, 3.0, 4.0, 5.0, 6.0, 7.0]
-ps = np.linspace(0.1, 1, num=11)
+bs = [1.01, 1.1, 1.5, 2.0, 2.5, 3.0, 4.0, 5.0, 6.0, 7.0]
+ps = np.linspace(0.1, 1, num=11)[7:8]
 a = 'binarytree'
 
 nruns = 1000
--- testPowerlaw.py
+++ testPowerlaw.py
@@ -5,11 +5,14 @@ import matplotlib.pyplot as plt
 
 vals1 = [v2.samplepowerlaw(exponent=0.01, minval=1) for x in range(100000)]
 vals2 = [v2.samplepowerlaw(exponent=0.10, minval=1) for x in range(100000)]
-vals3 = [v2.samplepowerlaw(exponent=0.90, minval=1) for x in range(100000)]
+vals3 = [v2.samplepowerlaw(exponent=2.10, minval=1) for x in range(100000)]
+vals4 = [v2.samplepowerlaw(exponent=3.20, minval=1) for x in range(100000)]
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
+ax = fig.add_subplot(211)
 ax.hist(vals1)
 ax.hist(vals2)
-ax.hist(vals3)
+ax = fig.add_subplot(212)
+ax.hist(np.log10(vals3), log=True)
+ax.hist(np.log10(vals4), log=True)
 plt.savefig('temp.pdf')
